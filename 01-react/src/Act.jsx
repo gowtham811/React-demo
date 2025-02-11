a874bc17e8374e605ca6a37413388a6734ac25a0
gowtham811 <dharanikotagowthamkumar@gmail.com>
2025-02-11 15:25:58 +0530

    2nd commit of React project with Vite

---
+++ 01-react/src/Act.jsx
@@ -0,0 +1,7 @@
+export function Act() {
+  return (
+    // This is a JSX comment
+      <h1>This is an another JSX file.</h1>
+    
+  )
+}
--- 01-react/src/main.jsx
+++ 01-react/src/main.jsx
@@ -1,9 +1,38 @@
-import { StrictMode } from 'react'
+import { createElement } from 'react';
+//import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+//import { Act } from './Act.jsx'
 
+const a="Shubham"
+function ToApp() {
+  return (
+    <h1>Hello World!:{a}</h1>
+  )
+}
+/*const reactElement={
+  type:'a',
+  props:{
+      href:'https://www.google.com',
+      target:"_blank"
+      
+  },
+  children:'Click me'
+}
+
+const reactElement2=(
+        <a href="https://www.google.com" target="_blank">
+          google here!
+        </a>
+)
+*/
+const reactElement=createElement(
+  'a',
+  {href:'https://www.google.com',target:'_blank'},
+  'Click here to visit google',
+  a
+)
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+  reactElement
 )
