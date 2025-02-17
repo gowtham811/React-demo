e5a1946fb2310d73067df0ca22e7cecf04f33cd8
gowtham811 <dharanikotagowthamkumar@gmail.com>
2025-02-17 12:51:38 +0530

    Todo Application

---
+++ todo/src/context/TodoContext.js
@@ -0,0 +1,24 @@
+import { use } from "react";
+import { createContext,useContext } from "react";
+export const Todocontext=createContext({
+    todos:[
+        {
+            id:1,
+            todo:"Todo msg",
+            completed:false,
+
+
+        }
+    ],
+    addTodo:(todo)=>{},
+    updateTodo:(id,todo)=>{},
+    deleteTodo:(id)=>{},
+    toggleComplete:(id)=>{}
+
+})
+
+export const useTodo=()=>{
+    return useContext(Todocontext)
+}
+
+export const TodoProvider=Todocontext.Provider
